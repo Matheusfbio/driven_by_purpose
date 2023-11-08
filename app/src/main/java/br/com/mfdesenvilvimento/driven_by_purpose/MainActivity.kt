5ab7d9c897eeef22877e7cf16cd3ce47e2b6b7e7
Matheus Fábio <fabiomatheus488@gmail.com>
2023-11-08 07:49:52 -0300

    get Started driven by purpose appp

---
+++ app/src/main/java/br/com/mfdesenvilvimento/driven_by_purpose/MainActivity.kt
@@ -0,0 +1,13 @@
+ package br.com.mfdesenvilvimento.driven_by_purpose
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.mfdesenvilvimento.driven_by_purpose.R
+
+
+ class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_main)
+    }
+}
